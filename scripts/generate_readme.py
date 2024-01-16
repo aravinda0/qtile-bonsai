37eadae3e0ddac3b1d1edcc2cd68e8b24840b61c
Aravinda Rao <maniacalace@gmail.com>
2024-01-16 23:39:02 +0530

    chore: populate docs from code into template to generate readme

---
+++ scripts/generate_readme.py
@@ -0,0 +1,77 @@
+# SPDX-FileCopyrightText: 2023-present Aravinda Rao <maniacalace@gmail.com>
+# SPDX-License-Identifier: MIT
+
+
+import ast
+import inspect
+import pathlib
+
+import jinja2
+from libqtile.layout.base import Layout
+
+from qtile_bonsai import Bonsai
+from qtile_bonsai.core.utils import rewrap
+
+
+t_readme_path = pathlib.Path("templates/README.template.md")
+readme_path = pathlib.Path("README.md")
+
+
+def main():
+    t_readme = jinja2.Template(t_readme_path.read_text())
+
+    config_options = get_config_options(Bonsai)
+    commands = get_exposed_comands(Bonsai)
+
+    readme = t_readme.render(
+        {
+            "config_options": config_options,
+            "commands": commands,
+        }
+    )
+
+    readme_path.write_text(readme)
+
+
+def get_config_options(layout_cls: type[Layout]) -> list:
+    config_options = []
+
+    for option in layout_cls.defaults:
+        description = option[2]
+        if description is None:
+            raise ValueError(f"The `{option[0]}` option is missing documentation")
+
+        config_options.append(
+            {
+                "name": option[0],
+                "default": option[1],
+                "description": rewrap(description, width=100, html_whitespace=True),
+            }
+        )
+
+    return config_options
+
+
+def get_exposed_comands(layout_cls: type[Layout]) -> list:
+    commands = []
+
+    def is_exposed_command(node):
+        expose_command_decorator = next(
+            (d for d in node.decorator_list if d.id == "expose_command"), None
+        )
+        if expose_command_decorator is not None:
+            docstring = ast.get_docstring(node)
+            if docstring is None:
+                raise ValueError(f"The `{node.name}` command is missing documentation")
+            commands.append(
+                {
+                    "name": node.name,
+                    "docstring": rewrap(docstring, width=100, html_whitespace=True),
+                }
+            )
+
+    v = ast.NodeVisitor()
+    v.visit_FunctionDef = is_exposed_command
+    v.visit(ast.parse(inspect.getsource(layout_cls)))
+
+    return commands
---
+++ templates/README.template.md
@@ -0,0 +1,44 @@
+<!-- 
+README.md is a generated file! 
+
+To make modifications, make sure you're editing `templates/README.template.md`.
+Then generate the README with `python scripts/generate_readme.py`
+-->
+
+
+# Qtile Bonsai
+
+[![CI Status](https://github.com/aravinda0/qtile-bonsai/actions/workflows/ci.yml/badge.svg?branch=master)](https://github.com/aravinda0/qtile-bonsai/actions?query=branch%3Amaster)
+[![codecov](https://codecov.io/gh/aravinda0/qtile-bonsai/branch/master/graph/badge.svg?token=O0PSWZMHM6)](https://codecov.io/gh/aravinda0/qtile-bonsai)
+[![License - MIT](https://img.shields.io/github/license/qtile/qtile.svg)](https://github.com/aravinda0/qtile-bonsai/blob/master/LICENSE.txt)
+
+-----
+
+### :construction: :construction: This is a work in progress :construction: :construction:
+
+<br/>
+
+
+## Introduction
+
+_Qtile Bonsai_ provides a flexible layout for the
+[qtile](https://github.com/qtile/qtile) tiling window manager that allows you to
+open windows as tabs, splits and even tabs inside splits.
+
+
+## Reference
+
+### Configuration
+
+| Option Name | Default Value | Description |
+| ---         | ---           | ---         |
+{% for config_option in config_options %}|`{{ config_option.name }}` | {{ config_option.default }} | {{ config_option.description }} |
+{% endfor %}
+
+
+### Commands
+
+| Command Name | Description |
+| ---          | ---         |
+{% for command in commands %}|`{{ command.name }}` | {{ command.docstring }} |
+{% endfor %}
