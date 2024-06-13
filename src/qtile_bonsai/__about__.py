f3163f71daae9b855b0fdc029321a886460fac53
Aravinda Rao <maniacalace@gmail.com>
2024-06-13 05:47:14 +0530

    chore: move version info to top level of root package and discard __about__.py

--- src/qtile_bonsai/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2023-present Aravinda Rao <maniacalace@gmail.com>
-# SPDX-License-Identifier: MIT
-
-__version__ = "0.1.0"
--- src/qtile_bonsai/__init__.py
+++ src/qtile_bonsai/__init__.py
@@ -1,5 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present Aravinda Rao <maniacalace@gmail.com>
 # SPDX-License-Identifier: MIT
 
-
 from qtile_bonsai.layout import Bonsai  # noqa: F401
+
+
+__version__ = "0.1.0"
